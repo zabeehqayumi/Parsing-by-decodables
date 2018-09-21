0c434fc0938338fd24dcc58e3c28f0e22a376383
Zabeehullah Qayumi <32587905+zabeehqayumi@users.noreply.github.com>
2018-09-21 18:11:21 -0400

    Parse Json file by decoder

---
+++ Parsing by decodables/Model.swift
@@ -0,0 +1,12 @@
+//
+//  Model.swift
+//  Parsing by decodables
+//
+//  Created by Zabeehullah Qayumi on 9/21/18.
+//  Copyright © 2018 Zabeehullah Qayumi. All rights reserved.
+//
+
+import Foundation
+
+
+
--- Parsing by decodables/ViewController.swift
+++ Parsing by decodables/ViewController.swift
@@ -8,11 +8,68 @@
 
 import UIKit
 
+struct Course : Decodable {
+    let id: Int
+    let name : String
+    let link : String
+    let imageUrl : String
+    
+    
+    init (json : [String : Any]){
+        
+        id = json["id"] as? Int ?? 1
+        name = json["name"] as? String ?? ""
+        link = json["link"] as? String ?? ""
+        imageUrl = json["imageUrl"] as? String ?? ""
+        
+        
+    }
+}
+
+
 class ViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let jsonUrl = "https://api.letsbuildthatapp.com/jsondecodable/course"
+        guard let url = URL(string: jsonUrl) else {return}
+        URLSession.shared.dataTask(with: url) { (data, response, err) in
+            // perhaps check the error
+            //check response status 200 OK
+            
+            
+            guard let data = data else {return}
+            
+//
+//            let dataAsString = String(data: data, encoding: .utf8)
+//            print(dataAsString)
+            
+            do{
+                
+                let course = try JSONDecoder().decode(Course.self, from: data)
+                print(course.name)
+                
+                
+                
+                
+                // tradition way of swift 2 and 3
+//                guard let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String:Any] else {return}
+//                let course = Course(json: json)
+//                print(course.name)
+//
+                
+            }catch{
+                print("Error serializing json : \(error.localizedDescription)")
+            }
+            
+            
+        
+            
+         
+        }.resume()
+
     }
 
 
