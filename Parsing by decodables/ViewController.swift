4564e7e7bb51d8bcce17515f3bddcdc873e8e076
Zabeehullah Qayumi <32587905+zabeehqayumi@users.noreply.github.com>
2018-09-21 17:29:53 -0400

    Initial Commit

---
+++ Parsing by decodables/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parsing by decodables
+//
+//  Created by Zabeehullah Qayumi on 9/21/18.
+//  Copyright © 2018 Zabeehullah Qayumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
